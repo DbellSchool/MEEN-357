a8471900d4596e327292ec2f7b9556eb5bb0c69b
DbellSchool <54284033+DbellSchool@users.noreply.github.com>
2021-09-08 11:36:05 -0500

    Initial commit

---
+++ Class One Practice.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 11:31:48 2021
+
+@author: ripti
+"""
+
+a = 2
+
+
+b = int(a)
+
+print(type(a))
+
+
+
+string_1= "tempest"
+String_2 = " Full temp ve"
+
+print(string_1+String_2)
+
+str_num = string_1.find("t")
+
+short_str = string_1[2:4]
+
+split_m = string_1.split('m')
+
+
+print("dir then name")
+
+print("ingo:\n",str_num,"\n",short_str,"\n",split_m)
+
+
+"""
+in python Stirng is imutable
+
+touples ar imutble
+
+
+list are mutable
+
+"""
+
+def run():
+    from stdent_joe import student
+    student_firstname= student['firstname]
+    
