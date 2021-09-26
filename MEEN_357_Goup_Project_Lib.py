7adb7312538fd3f6ed3000ff908d9f27f4d0812f
DbellSchool <54284033+DbellSchool@users.noreply.github.com>
2021-09-26 15:41:17 -0500

    Added dictionary and var

--- MEEN_357_Goup_Project_Lib.py
+++ MEEN_357_Goup_Project_Lib.py
@@ -1,1 +1,22 @@
-# project 
+# project Funcitons & libreary
+
+
+def get_mass(rover_dic):
+    #Computes the total mass of the rover. Uses information in the rover dict
+    rov_mass = 0
+
+
+    return rov_mass
+
+def get_gear_ratio(Speed_red_dic):
+    #returns the speed reduction ratio for the speed reducer based on speed_reducer dict.
+    sr_ratio = 0 
+
+    return sr_ratio
+
+def tau_dcmotor(v,MoterSpec_dic):
+    #Returns the motor shaft torque when given motor shaft speed and a dictionary containing important specifications for the motor. 
+    tourqe =0
+
+    return tourqe
+
---
+++ MEEN_357_Goup_Project_exe.py
@@ -0,0 +1,32 @@
+# Project exicution File
+
+wheel_rad = 0.3 # meters
+Wheel_mass = 1.0 # kg one drive wheel
+T_s = 170 #N*m
+T_nl = 0 #N*m
+w_nl = 3.80 # rad/s
+Motor_mass = 5.0 # kg ( one motor )
+Payload = 75 # kg ( one motor )
+RTG_mass = 90 # kg
+Chassis_mass = 659 # kg
+d1 = 0.04     #m Speed_reducer_minions
+d2 = 0.07     #m Speed reudcer gear diamiter
+SR_mass = 1.5 #kg
+g_mars = 3.72 # m/s
+
+
+
+# 
+wheel = {"radius":0.3, "mass": 1.0 }  #Radius in [m]  one drive wheel [kg]
+speed_reducer = {"type": "reverted", "diam_pinion": 0.04 ,"diam_gear":0.07, "mass":1.5} # diam in [m] mass in [kg]
+motor = {"torque_stall": 170, "torque_noload": 0, "speed_noload": 3.80, "mass": 5.0} #torque [N*m] speed [rad/s] mass [kg]
+
+
+chassis = {"mass": 695} #kg
+science_payload = {"mass":75 } #kg
+power_subsys = {"mass": 90} #kg
+planet = {"g":3.72} #m/s^2
+
+
+wheel_assembly = {"wheel": wheel, "speed_reducer": speed_reducer, "motor": motor}
+rover = {"wheel_assembly": wheel_assembly,"chassis":chassis, "science_payload":science_payload,"power_subsys":power_subsys }
