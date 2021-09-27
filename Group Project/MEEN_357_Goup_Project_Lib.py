995dcb4cce494316b5e0b9f937548a1d1a76fd1b
DbellSchool <54284033+DbellSchool@users.noreply.github.com>
2021-09-26 19:59:27 -0500

    updated files

--- Group Project/MEEN_357_Goup_Project_Lib.py
+++ Group Project/MEEN_357_Goup_Project_Lib.py
@@ -1,22 +1,46 @@
 # project Funcitons & libreary
 
 
-def get_mass(rover_dic):
+def get_mass(rov):
     #Computes the total mass of the rover. Uses information in the rover dict
-    rov_mass = 0
 
+    #print(rov)
+    wheels = rov['wheel_assembly']['wheel']['mass']*6
+    sRed = rov['wheel_assembly']['speed_reducer']['mass']*6
+    chassis = rov['chassis']['mass']
+    science_pay = rov['science_payload']['mass']
+    power_sub = rov['power_subsys']['mass']
+
+    rov_mass = wheels+ sRed+chassis+science_pay+power_sub
 
     return rov_mass
 
-def get_gear_ratio(Speed_red_dic):
+def get_gear_ratio(dic):
     #returns the speed reduction ratio for the speed reducer based on speed_reducer dict.
-    sr_ratio = 0 
+    d1= dic["diam_pinion"]
+    d2 = dic["diam_gear"]
+    Ng = (d2/d1)**2
 
-    return sr_ratio
+    #Ng = Ng *1
+
+
+    return Ng
 
 def tau_dcmotor(v,MoterSpec_dic):
     #Returns the motor shaft torque when given motor shaft speed and a dictionary containing important specifications for the motor. 
+
+  
     tourqe =0
 
     return tourqe
 
+
+def F_gravity(terrain_angle, rover, planet):
+    import numpy as np
+    terrain_angle = np.array([])
+    # need the mass of the entire rover
+    m = get_mass(rover)
+    mass = rover
+    rover("mass") 
+
+    return
