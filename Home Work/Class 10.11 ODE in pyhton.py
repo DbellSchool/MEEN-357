5125ce8d6a509a84035a7a9a6efe391c2de34512
DbellSchool <54284033+DbellSchool@users.noreply.github.com>
2021-10-11 12:11:03 -0500

    added class form 10.11

---
+++ Home Work/Class 10.11 ODE in pyhton.py
@@ -0,0 +1,8 @@
+# previously Explisate etho
+# Runge-huttea methosd in python (first order)
+# heun's mehtodd (secondn order) 
+from scipy.integrate import solve_ivp
+
+# deful is rk 24 --> 4th order rancarder ith 5th order step soze
+# Rk 23 secn order ankater methos wiht 3rd order method
+# DOP 853 ecplicit Runge-kutta method od oreder
